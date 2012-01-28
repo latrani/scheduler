42204c922537678e40d29020d909296fd6d3accd
Matt Thomas <matt+github@latrani.net>
2012-01-27 21:30:39 -0800

    Initial commit

---
+++ javascripts/app.js
@@ -0,0 +1,9 @@
+/*global Backbone */
+
+$(function() {
+    var Event = Backbone.Model.extend({
+        initialize: function() {
+            console.log("Initializing an event");
+        }
+    });
+});
