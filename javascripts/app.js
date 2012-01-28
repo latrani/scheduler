20d8c4260058a431f92597600f2da223c80e28cf
Matt Thomas <matt+github@latrani.net>
2012-01-28 15:49:42 -0800

    Some working interactive behavior

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,33 +1,113 @@
+/*global Backbone, Handlebars, _ */
+
+var Event = (function(){
+    var Model = Backbone.Model.extend({
+        // name: string
+        // track: string
+    });
+
+    var Collection = Backbone.Collection.extend({
+        model: Model
+    });
+
+    var SimpleTemplate = Handlebars.compile($("#event-template").html());
+
+    var SimpleView = Backbone.View.extend({  
+        render: function() { 
+            return $(this.el).html(SimpleTemplate(this.model.attributes));
+        }  
+    }); 
+
+    return {
+        model: Model,
+        collection: Collection,
+        view: SimpleView
+    };
+})();
+
+var Room = (function(){
+    var Model = Backbone.Model.extend({
+        initialize: function() {
+            this.events = new Event.collection();
+        }
+    });
+
+    var Collection = Backbone.Collection.extend({
+        model: Model
+    });
+
+    var SimpleTemplate = Handlebars.compile($("#room-template").html());
+
+    var SimpleView = Backbone.View.extend({
+        initialize: function() {
+            _.bindAll(this, "addEvent");
+            this.model.events.bind('add', this.addEvent);
+
+            // This part seems a bit inelegant...
+            var that = this;
+            $(this.el).on("click", "button.add-event", function(){
+                var name = prompt("Name?");
+                that.model.events.add(new Event.model({name: name}));
+            });
+        },
+        render: function() { 
+            return $(this.el).html(SimpleTemplate(this.model.attributes));
+        },
+        addEvent: function(event) {
+            $(this.el).find(".events").append(new Event.view({model: event}).render());
+        }
+    }); 
+
+    return {
+        model: Model,
+        collection: Collection,
+        view: SimpleView
+    };
+})();
+
+var Scheduler = (function(){
+    var Model = Backbone.Model.extend({
+        initialize: function() {
+            this.rooms = new Room.collection();
+        }
+    });
+
+    var View = Backbone.View.extend({  
+        initialize: function() {
+            this.el = $("#scheduler");
+            this.model.rooms.bind('add', this.addRoom);
+
+            // This part seems a bit inelegant...
+            var that = this;
+            $(this.el).find(".add-room").click(function(){
+                var name = prompt("Name?");
+                that.model.rooms.add(new Room.model({name: name}));
+            });
+        },
+
+        addRoom: function(room) {
+          $("#rooms").append(new Room.view({model: room}).render());
+        }
+    }); 
+
+    return {
+        initialize: function() {
+            var model = new Model();
+            var view = new View({model: model});
+            return {
+                model: model,
+                view: view
+            };
+        }
+    };
+})();
+
 $(function(){
-    /*global Backbone, Handlebars */
-    var Track = Backbone.Model.extend({
-    });
+    var scheduler = Scheduler.initialize();
 
-    var TrackList = Backbone.Collection.extend({
-        model: Track
-    });
+    var ballroom = new Room.model({name: "Ballroom"});
+    var dance = new Event.model({name: "Glow Dance"});
 
-    var Room = Backbone.Model.extend({
-    });
-
-    var RoomList = Backbone.Collection.extend({
-        model: Room
-    });
-
-    var Event = Backbone.Model.extend({
-    });
-
-    var EventList = Backbone.Collection.extend({
-        model: Event
-    });
-
-    var EventTemplate = Handlebars.compile($("#event-template").html());
-
-    var EventView = Backbone.View.extend({  
-      render : function() { 
-        $(this.el).html(EventTemplate());
-      }  
-    });  
-
-    var event1 = new Event({name: "Glow Dance"});
+    scheduler.model.rooms.add(ballroom);
+    ballroom.events.add(dance);
 });
