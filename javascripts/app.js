1bc2c2876efb1dec2046db81493536dbfec032d8
Matt Thomas <matt+github@latrani.net>
2012-01-28 01:20:03 -0800

    Adding some basic models

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,9 +1,33 @@
-/*global Backbone */
-
-$(function() {
-    var Event = Backbone.Model.extend({
-        initialize: function() {
-            console.log("Initializing an event");
-        }
+$(function(){
+    /*global Backbone, Handlebars */
+    var Track = Backbone.Model.extend({
     });
+
+    var TrackList = Backbone.Collection.extend({
+        model: Track
+    });
+
+    var Room = Backbone.Model.extend({
+    });
+
+    var RoomList = Backbone.Collection.extend({
+        model: Room
+    });
+
+    var Event = Backbone.Model.extend({
+    });
+
+    var EventList = Backbone.Collection.extend({
+        model: Event
+    });
+
+    var EventTemplate = Handlebars.compile($("#event-template").html());
+
+    var EventView = Backbone.View.extend({  
+      render : function() { 
+        $(this.el).html(EventTemplate());
+      }  
+    });  
+
+    var event1 = new Event({name: "Glow Dance"});
 });
