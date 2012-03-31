6e956a11176f75b847509b2210ff7a9f910fd9c2
Matt Thomas <matt+github@latrani.net>
2012-03-31 00:57:58 -0700

    Styling stuff

--- javascripts/app.js
+++ javascripts/app.js
@@ -5,8 +5,12 @@ $(function(){
     $("body").append(scheduler.view.render());
 
     var ballroom = new Room.model({name: "Ballroom"});
+    var panelroom = new Room.model({name: "Panel Room"});
     var dance = new Event.model({name: "Glow Dance"});
+    var panel = new Event.model({name: "Postfurry Panel"});
 
     scheduler.model.rooms.add(ballroom);
     ballroom.events.add(dance);
+    scheduler.model.rooms.add(panelroom);
+    panelroom.events.add(panel);
 });
